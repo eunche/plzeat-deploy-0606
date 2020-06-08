4bb37a56fcf8c9a7aac66b3796edddc2bd3f50a9
eunchae619 <eunchae619@naver.com>
2020-06-08 20:52:41 +0900

    사이드메뉴 밖 누르면 사라지게 js추가

--- core/static/core/js/menu.js
+++ core/static/core/js/menu.js
@@ -1,23 +1,46 @@
 // 변수 선언
-const menu = document.querySelector(".jsMenu")
-const hiddenMenu = document.querySelector(".jsHiddenMenu")
+const menu = document.querySelector(".jsMenu");
+const hiddenMenu = document.querySelector(".jsHiddenMenu");
+const jsHtml = document.querySelector("html");
 
 // 함수 정의
 
-const clickMenu = (event) => {
-    event.preventDefault();
-    if (hiddenMenu.classList.contains("jsClickedHiddenMenu")) {
-        hiddenMenu.classList.remove("jsClickedHiddenMenu")
-    } else {
-        hiddenMenu.classList.add("jsClickedHiddenMenu")
-    }
-}
+const clickOutside = (event) => {
+  if (
+    !hiddenMenu.classList.contains("jsClickedHiddenMenu") ||
+    event.target.classList.contains("power-off") ||
+    event.target.classList.contains("cog")
+  ) {
+    return;
+  }
+  event.preventDefault();
+  if (
+    !(
+      event.target.classList.contains("hidden_menu") ||
+      event.target.classList.contains("menu_top") ||
+      event.target.classList.contains("menu_bottom") ||
+      event.target.classList.contains("cog") ||
+      event.target.classList.contains("jsMenu") ||
+      event.target.classList.contains("power-off")
+    )
+  ) {
+    hiddenMenu.classList.remove("jsClickedHiddenMenu");
+  }
+};
 
+const clickMenu = (event) => {
+  event.preventDefault();
+  if (hiddenMenu.classList.contains("jsClickedHiddenMenu")) {
+    hiddenMenu.classList.remove("jsClickedHiddenMenu");
+  } else {
+    hiddenMenu.classList.add("jsClickedHiddenMenu");
+  }
+};
 
 const main = () => {
-    menu.addEventListener("click", clickMenu)
-}
-
+  menu.addEventListener("click", clickMenu);
+  jsHtml.addEventListener("click", clickOutside);
+};
 
 // 함수 호출
-main()
+main();
